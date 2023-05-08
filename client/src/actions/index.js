e4b16009aea04991119a652a32b56b5a15e2aa0a
Harshal Gosawi <harshalgosawi@gmail.com>
2023-05-08 20:42:39 +0530

    added server URL

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,31 +9,31 @@ import {
 } from './types';
 
 export const getAllTodo = () => async dispatch => {
-  const res = await axios.get('/api/todos');
+  const res = await axios.get('https://todos-z7y9.onrender.com/api/todos');
 
   dispatch({ type: GETALL_TODO, payload: res.data });
 };
 
 export const addNewTodo = name => async dispatch => {
-  const res = await axios.post('/api/todos', { name });
+  const res = await axios.post('https://todos-z7y9.onrender.com/api/todos', { name });
 
   dispatch({ type: ADDNEW_TODO, payload: res.data });
 };
 
 export const toggleTodo = id => async dispatch => {
-  const res = await axios.post(`/api/todos/${id}`);
+  const res = await axios.post(`https://todos-z7y9.onrender.com/api/todos/${id}`);
 
   dispatch({ type: TOGGLE_TODO, payload: res.data });
 };
 
 export const updateTodo = (id, name) => async dispatch => {
-  const res = await axios.patch(`/api/todos/${id}`, { name });
+  const res = await axios.patch(`https://todos-z7y9.onrender.com/api/todos/${id}`, { name });
 
   dispatch({ type: UPDATE_TODO, payload: { ...res.data, name } });
 };
 
 export const deleteTodo = id => async dispatch => {
-  const res = await axios.delete(`/api/todos/${id}`);
+  const res = await axios.delete(`https://todos-z7y9.onrender.com/api/todos/${id}`);
 
   dispatch({ type: DELETE_TODO, payload: res.data });
 };
